475eb3622b8f765420cfe1a8659162a13c0567e2
Ahmed Ghazy <ax3ghazy@aucegypt.edu>
2020-11-25 04:04:13 +0200

    Update caravel configs for singal routing

--- openlane/caravel/interactive.tcl
+++ openlane/caravel/interactive.tcl
@@ -11,17 +11,17 @@ init_floorplan
 
 add_macro_placement padframe 0 0 N
 add_macro_placement storage 280.650 263.920 N
-add_macro_placement soc 814.445 274.435 N
-add_macro_placement mprj 374.750 1279.800 N
-add_macro_placement mgmt_buffers 887.890 1158.940 N
-add_macro_placement rstb_level 826.815 1163.920 N
+add_macro_placement soc 1059.120 274.435 N
+add_macro_placement mprj 374.750 1349.705 N
+add_macro_placement mgmt_buffers 1066.855 1223.255 N
+add_macro_placement rstb_level 767.850 211.805 N
 add_macro_placement user_id_value 778.715 1158.940 N
-add_macro_placement por 2943.930 1167.355 N
+add_macro_placement por 905.435 1237.260 N
 
 # west
 # gpio_control_blocks: 37 ... 32
 set x 39.250
-set y 1090.740
+set y 994.760
 set pitch 223
 set orient N
 for {set i 37} {$i >= 32} {incr i -1} {
@@ -42,10 +42,10 @@ add_macro_placement "gpio_control_in\\\[24\\\]" $x $y $orient
 
 # east
 # gpio_control_bidir: 0 ... 1
-set x 3488.340
-set y 675.680
+set x 3373.015
+set y 588.645
 set pitch 233
-set orient N; # mirror
+set orient MY
 for {set i 0} {$i <= 1} {incr i} {
 	add_macro_placement "gpio_control_bidir\\\[$i\\\]" $x $y $orient
 	set y [expr {$y + $pitch}]
@@ -70,8 +70,8 @@ add_macro_placement "gpio_control_in\\\[14\\\]" $x $y $orient
 
 # north
 # gpio_control_in: 23 ... 15
-set x 509.535
-set y 5093
+set x 480.150
+set y 4979.065
 set pitch 273
 set orient R270
 for {set i 23} {$i >= 15} {incr i -1} {
@@ -82,9 +82,13 @@ for {set i 23} {$i >= 15} {incr i -1} {
 
 manual_macro_placement f
 
+# modify to a different file
+remove_pins -input $::env(CURRENT_DEF)
+remove_empty_nets -input $::env(CURRENT_DEF)
+
 # li1_hack_start
-# global_routing
-# detailed_routing
+global_routing
+detailed_routing
 # li1_hack_end
 
 run_magic
