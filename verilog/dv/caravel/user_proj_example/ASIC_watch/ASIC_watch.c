90f793bea9620c3cf6907c5e25de4602a77b90c4
Guillem <cabo.guillem@gmail.com>
2020-11-26 21:45:36 +0100

    add project 6 tb

---
+++ verilog/dv/caravel/user_proj_example/ASIC_watch/ASIC_watch.c
@@ -0,0 +1,60 @@
+#include "../../defs.h"
+
+#define PROJECT 6
+#define NUMNODES 6
+/*
+	IO Test:
+		- Configures MPRJ pins
+*/
+
+void main()
+{
+	/*
+	IO Control Registers
+	| DM     | VTRIP | SLOW  | AN_POL | AN_SEL | AN_EN | MOD_SEL | INP_DIS | HOLDH | OEB_N | MGMT_EN |
+	| 3-bits | 1-bit | 1-bit | 1-bit  | 1-bit  | 1-bit | 1-bit   | 1-bit   | 1-bit | 1-bit | 1-bit   |
+	Output: 0000_0110_0000_1110  (0x1808) = GPIO_MODE_USER_STD_OUTPUT
+	| DM     | VTRIP | SLOW  | AN_POL | AN_SEL | AN_EN | MOD_SEL | INP_DIS | HOLDH | OEB_N | MGMT_EN |
+	| 110    | 0     | 0     | 0      | 0      | 0     | 0       | 1       | 0     | 0     | 0       |
+	Output: 0000_0110_0000_1111  (0x1809) = GPIO_MODE_MGNT_STD_OUTPUT
+	| DM     | VTRIP | SLOW  | AN_POL | AN_SEL | AN_EN | MOD_SEL | INP_DIS | HOLDH | OEB_N | MGMT_EN |
+	| 110    | 0     | 0     | 0      | 0      | 0     | 0       | 1       | 0     | 0     | 1       |
+
+
+	Input: 0000_0001_0000_1111 (0x0402) = GPIO_MODE_USER_STD_INPUT_NOPULL
+	| DM     | VTRIP | SLOW  | AN_POL | AN_SEL | AN_EN | MOD_SEL | INP_DIS | HOLDH | OEB_N | MGMT_EN |
+	| 001    | 0     | 0     | 0      | 0      | 0     | 0       | 0       | 0     | 1     | 0       |
+	*/
+
+    /*
+    Inputs
+    system clock
+    system reset_n
+
+    Outputs
+    2 - 8  segment_hxxx
+    9 - 15 segment_xhxx
+    16 - 22 segment_xxhx
+    23 - 29 segment_xxxh
+    */
+	volatile uint32_t *io = &reg_mprj_io_0;
+	for (int i = 0; i < NUMNODES; i++) {
+		for (int j = 0; j <= 1; j += 6)
+			io[i + j] = GPIO_MODE_USER_STD_INPUT_NOPULL;
+		for (int j = 1; j <= 29; j += 6)
+			io[i + j] = GPIO_MODE_USER_STD_OUTPUT;
+	}
+
+    /* Apply configuration */
+    reg_mprj_xfer = 1;
+    while (reg_mprj_xfer == 1);
+
+    // change to project
+    reg_mprj_slave = PROJECT;
+
+    // use logic analyser bit 0 as reset
+    reg_la0_ena  = 0x00000000; // bits 31:0 outputs
+    reg_la0_data = 0x00000001; // reset high is on bit 0
+    reg_la0_data = 0x00000000; // low
+
+}
