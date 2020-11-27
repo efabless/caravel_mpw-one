740defbfb78892a0a0fd50a50364f7a7bede2c10
agorararmard <aagouhar@efabless.com>
2020-11-27 23:58:47 +0000

    [DATA] WIP mgmt_core rehardening
    - still has 7 TR vios - 6 shorts, which results in LVS mismatches.

--- openlane/mgmt_core/config.tcl
+++ openlane/mgmt_core/config.tcl
@@ -11,7 +11,9 @@ set ::env(FP_PDN_VPITCH) 50
 set ::env(PDN_CFG) $script_dir/pdn.tcl
 
 set ::env(FP_VERTICAL_HALO) 6
-set ::env(FP_PIN_ORDER_CFG) $script_dir/pin_order.cfg
+#set ::env(FP_PIN_ORDER_CFG) $script_dir/pin_order.cfg
+set ::env(FP_CONTEXT_DEF) $script_dir/../caravel/runs/caravel/tmp/floorplan/verilog2def_openroad.def
+set ::env(FP_CONTEXT_LEF) $script_dir/../caravel/runs/caravel/tmp/merged_unpadded.lef
 set ::env(FP_SIZING) absolute
 set ::env(DIE_AREA) "0 0 2150 850"
 
@@ -20,7 +22,7 @@ set ::env(MACRO_PLACEMENT_CFG) $script_dir/macro_placement.cfg
 set ::env(PL_TARGET_DENSITY) 0.52
 set ::env(PL_TARGET_DENSITY_CELLS) 0.38
 set ::env(PL_OPENPHYSYN_OPTIMIZATIONS) 1
-set ::env(CELL_PAD) 8
+set ::env(CELL_PAD) 4
 
 set ::env(GLB_RT_ADJUSTMENT) 0
 set ::env(GLB_RT_TILES) 14
