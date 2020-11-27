4ff45b1eb956675e9412f4c5ba913dac85a5931c
Tim Edwards <tim@opencircuitdesign.com>
2020-11-27 13:56:05 -0500

    Corrected the simple_por layout to avoid overlapping the capacitor plate of the top capacitor with the vias on the bottom capacitor. This corresponds to an update in the magic techfile in open_pdks to enforce the contact overlap prohibition.  Also updated the LVS run script to account for the change in directory path to the POR netlist.  Corrected the POR netlist to correct the dummy resistor size (they were specified in series instead of in parallel).  Re- extracted the POR and ran LVS.

--- mag/run_lvs_por.sh
+++ mag/run_lvs_por.sh
@@ -6,4 +6,4 @@
 # includes used by the testbench simulation are picked up.  Otherwise, the LVS
 # itself compares just the simple_por subcircuit from the testbench.
 #--------------------------------------------------------------------------------
-netgen -batch lvs "simple_por.spice simple_por" "../ngspice/simple_por_tb.spice simple_por" ~/projects/efabless/tech/SW/sky130A/libs.tech/netgen/sky130A_setup.tcl comp.out
+netgen -batch lvs "simple_por.spice simple_por" "../ngspice/simple_por/simple_por_tb.spice simple_por" ~/projects/efabless/tech/SW/sky130A/libs.tech/netgen/sky130A_setup.tcl comp.out
