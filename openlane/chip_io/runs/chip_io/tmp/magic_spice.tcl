e849be6ca5af8086405447303f4b04ca8870b269
Ahmed Ghazy <ax3ghazy@aucegypt.edu>
2020-11-30 23:01:26 +0200

    [DATA] chip_io current views

--- openlane/chip_io/runs/chip_io/tmp/magic_spice.tcl
+++ openlane/chip_io/runs/chip_io/tmp/magic_spice.tcl
@@ -1,12 +1,12 @@
 
-lef read /home/xrex/usr/devel/pdks/test/sky130A/libs.ref/sky130_fd_sc_hd/techlef/sky130_fd_sc_hd.tlef
+lef read /home/xrex/usr/devel/pdks/sky130A/libs.ref/sky130_fd_sc_hd/techlef/sky130_fd_sc_hd.tlef
 if {  [info exist ::env(EXTRA_LEFS)] } {
 	set lefs_in $::env(EXTRA_LEFS)
 	foreach lef_file $lefs_in {
 		lef read $lef_file
 	}
 }
-def read /project/openlane/chip_io/runs/chip_io/results/floorplan/padframe.def
+def read /project/openlane/chip_io/runs/chip_io/results/routing/chip_io.def
 load chip_io -dereference
 cd /project/openlane/chip_io/runs/chip_io/results/magic/
 extract do local
