28b76193390b2d34558b02415995bd9156543a24
Wojciech Gryncewicz <wgryncewicz@antmicro.com>
2020-12-08 15:41:15 +0100

    Added wavedrom Sphinx extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,6 +28,7 @@ author = 'efabless'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+  'sphinxcontrib.wavedrom',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
