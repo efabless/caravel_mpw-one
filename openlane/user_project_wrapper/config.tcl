0fbdaa5dec92622436922380406b6f47c03c0483
Ahmed Ghazy <ax3ghazy@aucegypt.edu>
2020-12-15 18:49:48 +0200

    Update user_project_wrapper to match the empty one

--- openlane/user_project_wrapper/config.tcl
+++ openlane/user_project_wrapper/config.tcl
@@ -58,13 +58,12 @@ set ::env(FILL_INSERTION) 0
 set ::env(TAP_DECAP_INSERTION) 0
 set ::env(CLOCK_TREE_SYNTH) 0
 
-# Need to fix a FastRoute bug for this to work, but it's good
-# for a sense of "isolation"
-set ::env(MAGIC_ZEROIZE_ORIGIN) 0
-set ::env(MAGIC_WRITE_FULL_LEF) 1
-
 
 # DON'T TOUCH THE FOLLOWING SECTIONS
+
+# This makes sure that the core rings are outside the boundaries
+# of your block.
+set ::env(MAGIC_ZEROIZE_ORIGIN) 0
 
 # Area Configurations. DON'T TOUCH.
 set ::env(FP_SIZING) absolute
@@ -74,16 +73,19 @@ set ::env(DIE_AREA) "0 0 2920 3520"
 set ::env(FP_PDN_CORE_RING) 1
 set ::env(FP_PDN_CORE_RING_VWIDTH) 3
 set ::env(FP_PDN_CORE_RING_HWIDTH) $::env(FP_PDN_CORE_RING_VWIDTH)
+set ::env(FP_PDN_CORE_RING_VOFFSET) 14
+set ::env(FP_PDN_CORE_RING_HOFFSET) $::env(FP_PDN_CORE_RING_VOFFSET)
 set ::env(FP_PDN_CORE_RING_VSPACING) 1.7
 set ::env(FP_PDN_CORE_RING_HSPACING) $::env(FP_PDN_CORE_RING_VSPACING)
-set ::env(FP_PDN_VOFFSET) 14
+
+set ::env(FP_PDN_VWIDTH) 3
+set ::env(FP_PDN_HWIDTH) 3
+set ::env(FP_PDN_VOFFSET) 0
 set ::env(FP_PDN_HOFFSET) $::env(FP_PDN_VOFFSET)
 set ::env(FP_PDN_VPITCH) 180
 set ::env(FP_PDN_HPITCH) $::env(FP_PDN_VPITCH)
 set ::env(FP_PDN_VSPACING) [expr 5*$::env(FP_PDN_CORE_RING_VWIDTH)]
 set ::env(FP_PDN_HSPACING) [expr 5*$::env(FP_PDN_CORE_RING_HWIDTH)]
-set ::env(FP_PDN_CORE_RING_VOFFSET) 0
-set ::env(FP_PDN_CORE_RING_HOFFSET) $::env(FP_PDN_CORE_RING_VOFFSET)
 
 set ::env(VDD_NETS) [list {vccd1} {vccd2} {vdda1} {vdda2}]
 set ::env(GND_NETS) [list {vssd1} {vssd2} {vssa1} {vssa2}]
@@ -102,6 +104,3 @@ set ::env(FP_IO_HLENGTH) $::unit
 
 set ::env(FP_IO_VTHICKNESS_MULT) 4
 set ::env(FP_IO_HTHICKNESS_MULT) 4
-
-
-
