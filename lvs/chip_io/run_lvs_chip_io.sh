d213394be116ed5f9ec09542f10bca0e2c4e2256
mkk <mkk@efabless.com>
2021-06-20 19:32:53 -0700

    Updated the LVS setup for both chip_io and caravel GL simulations are passing top-level caravel not lvs clean

--- lvs/chip_io/run_lvs_chip_io.sh
+++ lvs/chip_io/run_lvs_chip_io.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 #
 
-./maglef2lvs.sh chip_io.mag
-netgen -batch lvs "chip_io.maglef.lvs.lay.spice chip_io" "chip_io.v chip_io" sky130A_setup.tcl chip_io_comp.out
+cd ../../mag
+mv ../maglef/chip_io.mag ../maglef/chip_io.mag.tmp
+maglef2lvs.sh chip_io.mag
+mv ../maglef/chip_io.mag.tmp ../maglef/chip_io.mag
+cd ../lvs/chip_io
+
+netgen -batch lvs "chip_io.maglef.lay.spice chip_io" "../../verilog/gl/chip_io.v chip_io" sky130A_setup.tcl chip_io_comp.out
 cat chip_io_comp.out
