5085afa697a23ba1bc5a015248d170dcc6b0d624
Tim Edwards <tim@opencircuitdesign.com>
2021-09-23 12:38:45 -0400

    Added files for IRSIM simulation.

---
+++ irsim/gpio_control_block.tcl
@@ -0,0 +1,50 @@
+# IRSIM simulation for gpio_control_block
+# This is a simple/quick test to make sure IRSIM runs correctly on the
+# sky130A technology.
+
+l vssd
+l vssd1
+h vccd
+h vccd1
+settle 4
+
+analyzer
+
+ana serial_clock resetn serial_data_in serial_data_out
+
+l serial_clock
+l resetn
+
+l serial_data_in
+l user_gpio_in
+l user_gpio_in
+l mgmt_gpio_in
+
+
+s 20
+
+# Input clock emulation
+
+set core_freq 5.0	;# in MHz, half the rate of the SoC core clock
+
+set core_period [expr 1.0 / ($core_freq * 1.0E6)]
+set half_period_ns [expr 1.0E9 * $core_period / 2.0]
+
+every $half_period_ns {
+    if {[query serial_clock] == 0} {
+        printp 10
+    }
+    toggle serial_clock
+}
+
+s 20
+h resetn
+s 20
+
+s 500
+
+# Continue simulation manually from the command line, e.g.,
+# run "s 50000"
+#
+# Run "histflush" after every 50k steps or the computer will run out of
+# memory.
---
+++ irsim/translate.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+#
+import sys
+import re
+
+if len(sys.argv) < 2:
+    print('Usage:  translate.py file_in.sim file_out.sim')
+    sys.exit(1)
+
+print('Input:  ' + sys.argv[1])
+print('Output:  ' + sys.argv[2])
+
+with open(sys.argv[2], 'w') as ofile:
+    with open(sys.argv[1], 'r') as ifile:
+        for line in ifile:
+            tokens = line.split()
+            if tokens[0] == 'x':
+                lt = len(tokens)
+                if lt > 6:
+                    if tokens[-1] == 'sky130_fd_pr__pfet_01v8_hvt':
+                        tokens[0] = 'p'
+                    elif tokens[-1] == 'sky130_fd_pr__nfet_01v8':
+                        tokens[0] = 'n'
+                    else:
+                        print('Unrecognized device ' + tokens[-1])
+                    for i in range(1, lt):
+                        if '=' in tokens[i]:
+                            break
+                        elif len(tokens[i]) > 5:
+                            if tokens[i][-4:] == '/VPB':
+                                tokens[i] = 'VPWR'
+                            elif tokens[i][-4:] == '/VNB':
+                                tokens[i] = 'VGND'
+                            elif tokens[i][-5:] == '/VPWR':
+                                tokens[i] = 'VPWR'
+                            elif tokens[i][-5:] == '/VGND':
+                                tokens[i] = 'VGND'
+                    # Strip parameter name off of the four tokens in front of
+                    # the device name.
+                    tokens[-2] = tokens[-2][2:]
+                    tokens[-3] = tokens[-3][2:]
+                    tokens[-4] = tokens[-4][2:]
+                    tokens[-5] = tokens[-5][2:]
+                    newline = ' '.join(tokens[:-6])
+                    newline += ' ' + ' '.join(tokens[-5:-1])
+                    print(newline, file=ofile)
+            elif tokens[0] == 'r':
+                # Resistors---value gets output as zero (need to check why).
+                tokens[-1] = '200.0'
+                newline = ' '.join(tokens)
+                print(newline, file=ofile)
+            elif tokens[0] == 'D':
+                # Replace each diode with an equivalent diffusion capacitance
+                tokens[0] = 'C'
+                tokens.append('0.878')
+                newline = ' '.join(tokens)
+                print(newline, file=ofile)
+            else:
+                print(line, file=ofile, end='')
