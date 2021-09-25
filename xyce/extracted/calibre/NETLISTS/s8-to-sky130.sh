9f4470d07ed15215a6201257859956ecf1a60016
Mohamed Kassem <mkk@efabless.com>
2021-09-25 12:46:38 -0700

    added s8-to-sky130.sh

--- xyce/extracted/calibre/NETLISTS/s8-to-sky130.sh
+++ xyce/extracted/calibre/NETLISTS/s8-to-sky130.sh
@@ -3,7 +3,7 @@
  
 $SAK/bin/hspice2ngspice.sh chip_io.cdl  chip_io.cdl.NG
 $SAK/bin/subsnoempty.sh < chip_io.cdl.NG.unfolded > chip_io.cdl.NG.unfolded.no.empty.subckt
-$SAK/bin/spiUniquify < chip_io.cdl.NG.unfolded `./topsubs.sh chip_io.cdl.NG.unfolded` > chip_io.cdl.NG.unfolded.no.duplicates
+$SAK/bin/spiUniquify  chip_io.cdl.NG.unfolded `./topsubs.sh chip_io.cdl.NG.unfolded` > chip_io.cdl.NG.unfolded.no.duplicates
 
 
 
