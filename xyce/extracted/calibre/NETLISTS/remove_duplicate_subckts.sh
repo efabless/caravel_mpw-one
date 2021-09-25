bf9b40134c7140311f250742fc29705ce08f63d9
Mohamed Kassem <mkk@efabless.com>
2021-09-25 12:31:20 -0700

    converted chip_io to work with xyce/spice

--- xyce/extracted/calibre/NETLISTS/remove_duplicate_subckts.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-for x in `ls caravel-extracted.spice`
-do
-./unfold $x > $x.unfolded
-./spiUniquify $x.unfolded `./topsubs.sh $x.unfolded` > ${x%.spice}.xyce; 
-\rm *unfolded
-done
